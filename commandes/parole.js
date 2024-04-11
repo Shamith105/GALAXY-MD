589ce8b593ad1986e816bf573b164f199b4920e5
Shamith105 <161989688+Shamith105@users.noreply.github.com>
2024-04-11 08:42:13 +0530

    Update parole.js

--- commandes/parole.js
+++ commandes/parole.js
@@ -18,7 +18,7 @@ zokou({ nomCom: "lyrics",
 
     if (lyrics.error) return repondre("no lyrics found");
 
-    let msg = `---------MASTER-MD-lyrics-finder--------
+    let msg = `---------GALAXY-MD-lyrics-finder--------
 
 * *Artist :* ${lyrics.artist}
 
